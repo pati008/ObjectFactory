de9a2a02e404e06bc0beeac99171a1c0e32c89e4
pati008 <patrycja_pogorzelska26@milton.edu>
2023-10-25 09:02:08 -0400

    Made default pony and printed out unicorn

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Ponies p=new Ponies();
+        System.out.println(p.isUnicorn);
+        p.isUnicorn=true;
+        System.out.println(p.isUnicorn);
+
+    }
+
+
+}
---
+++ src/Ponies.java
@@ -0,0 +1,14 @@
+public class Ponies {
+    //true/false
+    // numbers -> int -> double
+    // words= string
+    boolean isUnicorn= false;
+    boolean isPegasus= false;
+
+    double size = 20.56;
+
+    String name= "bobby";
+    String color="pink";
+
+
+}
